837a5940c5f415cde31bb1b0b5d409403c74464a
Michael Hoff <hoffie.hoff@gmail.com>
2022-01-30 23:51:11 -0800

    update menu-items to navigate withRouter

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/Homepage';
+import Homepage from './pages/homepage/Homepage';
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- crwn-clothing/src/components/directory-menu/DirectoryMenu.jsx
+++ crwn-clothing/src/components/directory-menu/DirectoryMenu.jsx
@@ -51,8 +51,8 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size})=>{
-                        return <MenuItem title = {title} key = {id} imageUrl={imageUrl} size={size}/>;
+                    this.state.sections.map(({id, ...otherSectionProps})=>{
+                        return <MenuItem key = {id} {...otherSectionProps}/>;
                     })
                 }
             </div>
