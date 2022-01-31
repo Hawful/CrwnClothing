b5c6bbff594886ce12304eb10698351263fd36b2
Michael Hoff <hoffie.hoff@gmail.com>
2022-01-31 11:50:47 -0800

    added collection components with basic stying

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+import Shop from './pages/shop/Shop';
 
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
-  </div>
-);
 
 function App() {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   );
