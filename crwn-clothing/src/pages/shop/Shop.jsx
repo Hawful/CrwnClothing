666925d18a3ceec88270b6bd730e91bf9921e975
Michael Hoff <hoffie.hoff@gmail.com>
2022-02-08 08:05:18 -0800

    Added persist for local storage, added redux to shop and menu data

--- crwn-clothing/src/pages/shop/Shop.jsx
+++ crwn-clothing/src/pages/shop/Shop.jsx
@@ -1,28 +1,12 @@
 import React from "react";
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
 
-import SHOP_DATA from "./shop-data";
-
-import CollectionPreview from "../../components/collection-preview/CollectionPreview";
-
-class Shop extends React.Component {
-    state = {
-        collections: SHOP_DATA
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps }) =>{
-                        return <CollectionPreview key={id} {...otherCollectionProps}/>;
-                    })
-                }
-            </div>
+const Shop = () => (
+    <div className="shop-page">
+        <CollectionsOverview/>
+    </div>
+)
 
 
-        )
-    }
-}
 
 export default Shop;
