d71e918070ee1488d108e87a2b1c4909eef78490
Michael Hoff <hoffie.hoff@gmail.com>
2022-02-12 23:58:47 -0800

    Added styled components and styled homepage

--- crwn-clothing/src/pages/homepage/Homepage.jsx
+++ crwn-clothing/src/pages/homepage/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import './Homepage.scss';
 import Directory from '../../components/directory-menu/DirectoryMenu'
+import { HomePageContainer } from "./homepage-styles";
 
 const HomePage = () => {
 
     return(
-        <div className="homepage">
+        <HomePageContainer>
             <Directory/>
-        </div>
+        </HomePageContainer>
     ); 
 }
 
